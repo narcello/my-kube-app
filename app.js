0a6f7f1d18607ebde3e2869c8a39e4008429a194
narcello <marcellovcs@gmail.com>
2025-07-27 11:14:45 -0300

    new version - v6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from Kubernetes! - v5"));
+app.get("/", (req, res) => res.send("Hello from Kubernetes! - v6"));
 app.listen(3000);
